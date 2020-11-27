07e7d1279b040439390b8516ed411f5852db873f
Martijn van de Rijdt <martijn@enketo.org>
2020-11-27 13:36:32 -0700

    consistent override method

--- src/js/xpath-evaluator-binding.js
+++ src/js/xpath-evaluator-binding.js
@@ -4,27 +4,8 @@ import OpenRosaXPath from 'openrosa-xpath-evaluator';
 
 /**
  * @function xpath-evaluator-binding
- * @param {Function} addExtensions - extension function
  */
-export default function( addExtensions ) {
+export default function( ) {
     const evaluator = OpenRosaXPath();
-
-    /*
-     * Note: it's inefficient to extend XPathJS here (for every model instance)
-     * instead of just once in the prototype.
-     *
-     * However, this is done to prevent breaking Medic Mobile.
-     * The performance impact is probably negligible, since we don't instantiate
-     * models very often.
-     *
-     * In any case, you don't have to use it like this. It was done for
-     * Enketo Validate only. In an app that doesn't override enketo-xpathjs,
-     * I'd recommend using `require('extension')(require('enketo-xpathjs'))` instead
-     * and leave the addExtensions parameter empty here.
-     */
-    if ( typeof addExtensions === 'function' ) {
-        addExtensions( evaluator );
-    }
-
     this.xml.jsEvaluate = evaluator.evaluate;
 }
